807725c84fe4f92b2aaabe535e06a7234bbf3567
doyoon <doyoun0527@naver.com>
2024-04-11 17:10:37 +0900

    엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //order 테이블에 있는 member 필드에 의해 mapping 된거야.
+    private List<Order> orders = new ArrayList<>();
+
+}
